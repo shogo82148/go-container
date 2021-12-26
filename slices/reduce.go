8fd112b234f1e1f4b7a5541658feadc23dae4666
Ichinose Shogo <shogo82148@gmail.com>
2021-12-26 19:10:07 +0900

    fix types in slices.Reduce

--- slices/reduce.go
+++ slices/reduce.go
@@ -1,18 +1,6 @@
 package slices
 
-func Reduce[T any](a []T, f func(T, T) T) T {
-	if len(a) == 0 {
-		var zero T
-		return zero
-	}
-	ret := a[0]
-	for _, v := range a[1:] {
-		ret = f(ret, v)
-	}
-	return ret
-}
-
-func ReduceInit[T any](a []T, init T, f func(T, T) T) T {
+func Reduce[T, U any](a []T, init U, f func(U, T) U) U {
 	ret := init
 	for _, v := range a {
 		ret = f(ret, v)
